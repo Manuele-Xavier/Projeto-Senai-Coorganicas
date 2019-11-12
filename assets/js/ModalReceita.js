f28e244257c62ea4c87515c07cd6b316e59cf185
FIC <FIC@info.local>
2019-11-12 12:17:47 -0300

    modal receitas

---
+++ assets/js/ModalReceita.js
@@ -0,0 +1,10 @@
+function mudarConteudo(id) {
+    
+    var teste2 = document.getElementById(id);
+
+    var teste = document.getElementById("conteudo");
+
+    teste.innerHTML = teste2.innerHTML;
+
+   
+}
